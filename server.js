9cb1f3918091d9546aab488a37871baf3315be11
DJMIKE20 <m.zimm20@gmail.com>
2022-09-25 16:47:48 -0400

    query the database to test the connection

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,11 +7,26 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username
+        user: 'root',
+        // Your MySQL password
+        password: 'Indoorsql20$',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 
-// Default response for any other request (Not Found)
+// Default response for any other request (Not Found), catchall route
 app.use((req,res) => {
     res.status(404).end();
 })
