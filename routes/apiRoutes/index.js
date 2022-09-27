cdc7e8a0ba9d2ef698abf81b7ea43d0421a322b3
DJMIKE20 <m.zimm20@gmail.com>
2022-09-27 14:19:03 -0400

    modularize code for routes

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,10 @@
+// index.js will act as a central hub to pull them all together
+
+const express = require('express');
+const router = express.Router();
+
+router.use(require('./candidateRoutes'));
+router.use(require('./partyRoutes'));
+
+
+module.exports = router;
